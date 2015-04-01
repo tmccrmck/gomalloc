3c6e18657ba3755f17c676782c2b903c7256922d
trent <tjmcc01@gmail.com>
2015-04-01 16:22:10 -0700

    dealing with C pointer types

--- gmalloc.go
+++ gmalloc.go
@@ -3,10 +3,12 @@ package main
 
 /*
 #include <unistd.h>
+#include <stdint.h>
 */
 import "C"
 import "unsafe"
 import "fmt"
+import "errors"
 
 type s_block struct {
 	size int
@@ -19,6 +21,7 @@ type s_block struct {
 var base *s_block
 const BLOCK_SIZE int = 40
 
+/* Split the block struct by size 'size'*/
 func split_block(block s_block, size int){
 	var split s_block
 	block.size = size
@@ -28,17 +31,24 @@ func split_block(block s_block, size int){
 	split.size = block.size - size - BLOCK_SIZE
 }
 
+/* Extend heap by size size */
 func extend_heap(last s_block, size int) s_block{
-	var block s_block
-	var newEnd int
-	block = C.sbrk(0)
+	var block *s_block
+	var newEnd *int
+	var extend_length int = BLOCK_SIZE + size
+	block = (*s_block)(C.sbrk(0))
+	/* NEW END OF DATA */
+	newEnd = (*int)(C.sbrk(extend_length))
+	if *newEnd < 0 {
+		err := errors.New("sbrk fails")
+	}
 	block.size = size
 	block.prev = &last
 	block.free = 0
 	
-	return block
+	return *block
 }
-
+/* Allocate memory */
 func gmalloc(size int) unsafe.Pointer{
 	return unsafe.Pointer(base)
 }
