feef555035f15d1fb61c9a03457725e1ed141bd6
trent <tjmcc01@gmail.com>
2015-04-01 15:47:12 -0700

    finished extend and split

--- gmalloc.go
+++ gmalloc.go
@@ -17,33 +17,35 @@ type s_block struct {
 }
 
 var base *s_block
+const BLOCK_SIZE int = 40
 
-func split_block(block s_block, size int){
-
+func split_block(block s_block, size int64){
+	var split s_block
+	block.size = size
+	split.next = block.next
+	split.prev = &block
+	split.free = 1
+	split.size = block.size - size - BLOCK_SIZE
 }
 
 func extend_heap(last s_block, size int) s_block{
 	var block s_block
-
+	var newEnd int
+	block = C.sbrk(0)
+	block.size = size
+	block.prev = &last
+	block.free = 0
+	
 	return block
 }
 
-func malloc(size int) unsafe.Pointer{
+func gmalloc(size int) unsafe.Pointer{
 	return unsafe.Pointer(base)
 }
 
 func main(){
 	data := new(int)
-	data = malloc(4)
+	data = (*int) (gmalloc(4))
 	fmt.Println("Malloc sanity test passes")
 }
-
-
-
-
-
-
-
-
-
 
